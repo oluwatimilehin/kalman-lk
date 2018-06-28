4b93529d952402c8d183e74438d1b68b93a27e11
Timi <oluwatimilehinadeniran@gmail.com>
2018-06-28 10:07:26 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+
+
+def run(source):
+    cap = cv2.VideoCapture(source)
+
+    print("Press 'p' to pause the video and start tracking")
+    while True:
+        ret, img = cap.read()
+        if cv2.waitKey(10) == ord('p'):
+            break
+        cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
+        cv2.imshow("Image", img)
+    cv2.destroyWindow('Image')
+
+
+
+
+
+if __name__ == '__main__':
+    source = 'three-four.mp4'
+    run(source)

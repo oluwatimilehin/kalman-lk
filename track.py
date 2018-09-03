83c50446f89d876cd15a239acec001996cebf773
Timi <oluwatimilehinadeniran@gmail.com>
2018-09-03 19:15:07 +0100

    Use lucas kanade only when no results

--- track.py
+++ track.py
@@ -31,6 +31,24 @@ class Tracker:
     def run(self):
         self.measure()
 
+        x = []
+        y = []
+
+        for i, value in enumerate(self.measured):
+            x.append(value[0])
+            y.append(value[1])
+
+        x = np.array(x)
+        y = np.array(y)
+
+        x_min, x_max = np.min(x), np.max(x)
+        y_min, y_max = np.min(y), np.max(y)
+
+        top_x = math.ceil(x_min)
+        top_y = math.ceil(y_min)
+        bottom_x = math.ceil(x_max)
+        bottom_y = math.ceil(y_max)
+
         self.bgsubtractor.run(self.im2)
 
         if len(self.bgsubtractor.contours) > 0:
@@ -38,5 +56,6 @@ class Tracker:
 
             if len(rect_coordinates) > 0:
                 self.rect = self.bgsubtractor.get_best_candidate(rect_coordinates, self.measured)
-
+        else:
+            self.rect = Rect(top_x, top_y, bottom_x, bottom_y)
 
